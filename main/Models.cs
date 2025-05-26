c9bbb0cd0a58150b7c65d8f0e31f662e9b230c1a
VlAtrosh <Vlasosva99@gmail.com>
2025-05-26 12:09:53 +0300

    add

---
+++ main/Models.cs
@@ -0,0 +1,24 @@
+﻿public class Book
+{
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public string ISBN { get; set; }
+    public decimal Price { get; set; }
+    public string Publisher { get; set; }
+    public DateTime? PublicationDate { get; set; }
+}
+
+public class BookWithAuthor : Book
+{
+    public int AuthorId { get; set; }
+    public string AuthorFirstName { get; set; }
+    public string AuthorLastName { get; set; }
+}
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Biography { get; set; }
+}
